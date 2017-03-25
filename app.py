e976ecb3fa366bb4fee6f58469ff303eebee7ac0
Russell <sell_nat@yahoo.com>
2017-03-25 15:50:32 -0700

    Added base project

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+UPLOAD_FOLDER = './static/uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        args = []
+        args.append(request.form['firstname'])
+        args.append(request.form['lastname'])
+        return render_template('index.html', args=args)
+    else:
+        return render_template('index.html')
+
+@app.route('/type_of_food', methods=['GET','POST'])
+def type_of_food():
+    if request.method == 'POST':
+        args = []
+        args.append(request.form['firstname'])
+        args.append(request.form['lastname'])
+        return render_template('type_of_food.html', args=args)
+    else:
+        return render_template('type_of_food.html')
+
+
+@app.route('/food_survival', methods=['GET','POST'])
+def food_survival():
+    if request.method == 'POST':
+        args = []
+        args.append(request.form['firstname'])
+        args.append(request.form['lastname'])
+        return render_template('food_survival.html', args=args)
+    else:
+        return render_template('food_survival.html')
+
+
+@app.route('/storage_location', methods=['GET','POST'])
+def storage_location():
+    if request.method == 'POST':
+        args = []
+        args.append(request.form['firstname'])
+        args.append(request.form['lastname'])
+        return render_template('storage_location.html', args=args)
+    else:
+        return render_template('storage_location.html')
+
+@app.route('/forecast', methods=['GET','POST'])
+def forecast():
+    if request.method == 'POST':
+        args = []
+        args.append(request.form['firstname'])
+        args.append(request.form['lastname'])
+        return render_template('forecast.html', args=args)
+    else:
+        return render_template('forecast.html')
+
+
+
+
+
+
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+if __name__ == "__main__":
+    app.run(debug=True);
