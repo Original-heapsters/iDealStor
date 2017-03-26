a3539203d4120d085fa52093cc591f2834630b11
Russell <sell_nat@yahoo.com>
2017-03-25 23:54:18 -0700

    Added session capability

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from type_of_food import TypeOfFood
 from food_survival import FoodSurvival
 from storage_location import StorageLocation
 from forecast import Forecast
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g, session
 from flask.ext.googlemaps import GoogleMaps
 
 
@@ -13,6 +13,7 @@ GoogleMaps(app, key="AIzaSyDnDdKk8h8ipdZFyLMBeCUSbdPcShUNQjI")
 UPLOAD_FOLDER = './static/uploads/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'some key for session'
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -36,9 +37,34 @@ def type_of_food():
         args = []
         for item in request.form.getlist('crops'):
             args.append(item)
+        selectedCrops = {}
+
+        for key,value in crops.items():
+            for v in value:
+                for crop,cropData in v.items():
+                    if cropData["name"] in args:
+                        selectedCrops[crop] = {}
+                        print crop
+                        for tempStat in cropData["temperatures"]:
+                            selectedCrops[crop][tempStat] = cropData["temperatures"][tempStat]
+                        #print tempStat
+                        #print cropData["temperatures"][tempStat]
+
+        for k,v in selectedCrops.items():
+            for key, val in selectedCrops[k].items():
+                print k + " -- " + key + " -- " + val
+
+        session['selectCrops'] = selectedCrops
+
 
         return render_template('type_of_food.html', args=args, crops=crops)
     else:
+        if 'selectCrops' in session:
+            selCrops = session['selectCrops']
+            if len(selCrops) > 0:
+                for k,v in selCrops.items():
+                    for key, val in selCrops[k].items():
+                        print "SELECTED" + k + " -- " + key + " -- " + val
         return render_template('type_of_food.html', crops=crops)
 
 
